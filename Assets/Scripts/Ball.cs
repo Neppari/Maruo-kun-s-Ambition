36ee36acccee727c43a31f415c8bf6ddb77be68f
sekopallo <sekopallo@gmail.com>
2020-05-24 17:09:03 +0300

    Ball physics and testing setup

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [SerializeField] private float bounceMultiplier = default;
+    [SerializeField] private Vector3 startVelocity = default;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(startVelocity, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var impulse = collision.impulse;
+        rb.AddForce(impulse * bounceMultiplier, ForceMode.Impulse);
+    }
+}
